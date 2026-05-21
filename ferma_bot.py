7caf19860fc4fed21626371cf5f979a9936abdf2
inomjondjjcjd-source <inomjondjjcjd@gmail.com>
2026-05-21 19:52:48 +0500

    Update ferma_bot.py

--- ferma_bot.py
+++ ferma_bot.py
@@ -1,9 +1,9 @@
-Import os, json, random, time, urllib.request, telebot
+import os, json, random, time, urllib.request, telebot
 from flask import Flask
 from threading import Thread
 from telebot import types
 
-TOKEN = "8691200742:AAEv-8-wixOxzlHmIU-jbMy4QHYOE1-M6QM"
+TOKEN = "8565925340:AAEYU8A_rthlecqIWJjwJelIlsWM0L7A1_4"
 ADMIN_ID = 8086545587
 KVDB_URL = "https://kvdb.io/MN86yM86yM86yM86yM86yM/shox_sup_v10_db"
 
@@ -50,7 +50,7 @@ def start_cmd(message):
     uid = message.from_user.id
     ud = check_user(uid, message.from_user.first_name)
     ud["state"] = None; save_db()
-    txt = f"👑 *SHOX SUPREME PLATFORMA v10.0*\n\n💵 *Balans:* {ud['balance']} so'm\nStatus: 🟢 Live (Uyg'oq)"
+    txt = f"👑 *inomjon uzbekistan bot v10.0*\n\n💵 *Balans:* {ud['balance']} so'm\nStatus: 🟢 Live (Uyg'oq)"
     bot.send_message(message.chat.id, txt, parse_mode="Markdown", reply_markup=get_main_keyboard(uid))
 
 @bot.message_handler(func=lambda m: check_user(m.from_user.id).get("state") is not None)
@@ -267,5 +267,5 @@ if __name__ == '__main__':
     load_db(); port = int(os.environ.get("PORT", 10000))
     Thread(target=lambda: app.run(host="0.0.0.0", port=port), daemon=True).start()
     bot.infinity_polling(skip_pending=True)
+
         
-Men shu kiddan foydalanyamma shunga qisqaro qilib bot nomini martin live qilib ber va bot yana uhlab qolyapti umman uhlamidgan qilib ber aniq ujlamidga ql
